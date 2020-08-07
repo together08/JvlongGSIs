f6484a2ab172f730fc8dd1b9575951c644021eab
together08 <together08@yeah.net>
2020-08-07 22:14:40 +0800

    pack.sh: Also make Aonly image

--- pack.sh
+++ pack.sh
@@ -1,15 +1,20 @@
 echo "Start Packing"
 echo ""
 dirsize="$(du -sm "baseromdir"/system | awk '{print $1}' | sed 's/$/& (MB)/')"
-size=$dirsize + 130
+size=$dirsize+130
 cd "$LOCALDIR"
-
-
-# mke2fs+e2fsdroid Pack
 time=$(date "+%Y%m%d")
-packimg="$romname"-JvlongGSI-$time.img
-$bindir/mke2fs -t ext4 -b 4096 "$outdir"/"packimg" $size
-$bindir/e2fsdroid -e -T 0 -S "baseromdir"/config/system_file_contexts -C "baseromdir"/config/system_fs_config  -a /system -f "baseromdir"/system "$outdir"/"packimg"
-echo "Packing Finished."
+
+# AB Pack
+packimg="$romname"-JvlongGSI-AB-$time.img
+$bindir/mke2fs -t ext4 -b 4096 "$outdir"/$packimg $size
+$bindir/e2fsdroid -e -T 0 -S "baseromdir"/config/system_file_contexts -C "baseromdir"/config/system_fs_config  -a /system -f "baseromdir"/system "$outdir"/$packimg
+echo "AB GSI Packing Finished."
 echo "Path: $outdir/$packimg"
 
+# Aonly Pack
+packimg="$romname"-JvlongGSI-AB-$time.img
+$bindir/mke2fs -t ext4 -b 4096 "$outdir"/$packimg $size
+$bindir/e2fsdroid -e -T 0 -S "baseromdir"/config/system_file_contexts -C "baseromdir"/config/system_fs_config  -a /system -f "baseromdir"/system/system "$outdir"/$packimg
+echo "Aonly GSI Packing Finished."
+echo "Path: $outdir/$packimg"
