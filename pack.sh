a24bbaa7caacc7c3bb981b0371563d405211e97f
jvlong520 <together08@yeah.net>
2020-08-08 16:41:55 +0800

    pack.sh: AB -> Aonly

--- pack.sh
+++ pack.sh
@@ -13,7 +13,7 @@ echo "AB GSI Packing Finished."
 echo "Path: $outdir/$packimg"
 
 # Aonly Pack
-packimg="$romname"-JvlongGSI-AB-$time.img
+packimg="$romname"-JvlongGSI-Aonly-$time.img
 $bindir/mke2fs -t ext4 -b 4096 "$outdir"/$packimg $size
 $bindir/e2fsdroid -e -T 0 -S "baseromdir"/config/system_file_contexts -C "baseromdir"/config/system_fs_config  -a /system -f "baseromdir"/system/system "$outdir"/$packimg
 echo "Aonly GSI Packing Finished."
