70ee72e7d0fa4f09c10313fdc95c9a5602a07b20
together08 <2958537424@qq.com>
2020-08-16 15:59:55 +0800

    Fix bug

--- make.sh
+++ make.sh
@@ -14,17 +14,17 @@ if [ "$2" == "" ]; then
     exit 1
 fi
 
-LOCALDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+export LOCALDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 mkdir output
-HOST="$(uname)"
-rompath="$1"
-romname="$2"
-tmpdir="$LOCALDIR/temp"
-scriptdir="$LOCALDIR/scripts"
-toolsdir="$LOCALDIR/tools"
-bindir="$toolsdir/bin/$HOST"
-imgextractor="$toolsdir/imgextractor/imgextractor.py"
-outdir="$LOCALDIR/output"
+export HOST="$(uname)"
+export rompath="$1"
+export romname="$2"
+export tmpdir="$LOCALDIR/temp"
+export scriptdir="$LOCALDIR/scripts"
+export toolsdir="$LOCALDIR/tools"
+export bindir="$toolsdir/bin/$HOST"
+export imgextractor="$toolsdir/imgextractor/imgextractor.py"
+export outdir="$LOCALDIR/output"
 
 echo "Update tools..."
 git pull
@@ -46,7 +46,7 @@ echo ""
 echo ""
 echo "ErfanGSI Make Finished."
 echo "Copy ErfanGSI's GSI."
-erfan_product="$(ls "./erfan-tools/output/" | grep -i "ErfanGSI" | grep "img" | grep "AB")"
+export erfan_product="$(ls "./erfan-tools/output/" | grep -i "ErfanGSI" | grep "img" | grep "AB")"
 cp ./erfan-tools/output/$erfan_product "$tmpdir"
 cd "$tmpdir"
 mv $erfan_product erfangsi.img
@@ -54,8 +54,8 @@ cd ..
 echo "Starting JvlongGSIs Make..."
 
 
-baseromdir="$tmpdir/base-rom"
-erfandir="$tmpdir/erfangsi"
+export baseromdir="$tmpdir/base-rom"
+export erfandir="$tmpdir/erfangsi"
 
 # Mount system.img and copy files
 bash ./unpack.sh "$rompath" "$tmpdir"
@@ -66,20 +66,20 @@ mv system base-rom
 python3 $imgextractor "$tmpdir"/erfangsi.img "$tmpdir"
 
 # Get PT Info
-sourcetype="Aonly"
+export sourcetype="Aonly"
 if [ -d "$baseromdir/system" ]; then
-    sourcetype="AB"
+    export sourcetype="AB"
 fi
 
 # Change bin dir with sourcetype
 if [ "$sourcetype" == "Aonly" ]; then 
-	baserombin="$baseromdir/bin"
+	export baserombin="$baseromdir/bin"
 else
-	baserombin="$baseromdir/system/bin"
+	export baserombin="$baseromdir/system/bin"
 fi
 
 # Copy bin files
-erfanbin="$erfandir/system/bin"
+export erfanbin="$erfandir/system/bin"
 cp -n "$erfanbin"/* "$baserombin"
 
 # Package The GSI
